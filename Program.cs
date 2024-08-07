62444170023ca2abeeed00f6a6b5313ccd741426
Wren <grahamgrasley@gmail.com>
2024-08-07 16:55:36 -0400

    Cleanup
    Improve formatting, switch int to uint

--- Program.cs
+++ Program.cs
@@ -4,41 +4,42 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		const string format = "{0,7}\t{1,7}\t{2,7}";
-		int max_value = (int)Math.Cbrt(int.MaxValue);
+		const string format = "{0,8:N0}\t{1,9:N0}\t{2,13:N0}";
+		uint max_value = (uint)Math.Cbrt(uint.MaxValue);
 
 		Console.WriteLine("Learn your squares and cubes!\n");
 
 		do
 		{
 			Console.WriteLine("Please enter a number:");
-			int count = GetInput(1, max_value);
+			uint count = GetInput(1, max_value);
 
-			Console.WriteLine("\nNumber\tSquared\tCubed");
-			Console.WriteLine("=======\t=======\t=======\n");
-			for (int i = 1; i <= count; i++)
+            Console.WriteLine();
+            Console.WriteLine(string.Format(format, "Number", "Squared", "Cubed"));
+			Console.WriteLine(string.Format(format, "=======", "=======", "======="));
+			for (uint i = 1; i <= count; i++)
 				Console.WriteLine(string.Format(format, i, GetSquared(i), GetCubed(i)));
 
 		} while (GetContinue("Would you like to continue?"));
 	}
 
-	static int GetInput(int min, int max)
+	static uint GetInput(uint min, uint max)
 	{
-		int result = int.Parse(Console.ReadLine());
+		uint result = uint.Parse(Console.ReadLine());
 		while (result < min || result > max)
 		{
 			Console.WriteLine($"Please enter a number between {min} and {max}");
-			result = int.Parse(Console.ReadLine());
+			result = uint.Parse(Console.ReadLine());
 		}
 		return result;
 	}
 
-	static int GetSquared(int num)
+	static uint GetSquared(uint num)
 	{
 		return num * num;
 	}
 
-	static int GetCubed(int num)
+	static uint GetCubed(uint num)
 	{
 		return num * num * num;
 	}
